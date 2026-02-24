f0bb5d79182e16f7142d8f2d59877ab2c15d96ee
moazwaelamer <moazwaelamerr@gmail.com>
2026-02-24 06:19:50 +0200

    update

--- src/pages/Product/productDetatils.jsx
+++ src/pages/Product/productDetatils.jsx
@@ -133,7 +133,24 @@ export default function ProductView() {
 
         {/* Toast */}
         {/* Toast */}
-{toast && <div className="cart-toast show">{toast}</div>}
+{toast && (
+  <div className="cart-toast show">
+    <span className="toast-icon">
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M20 6L9 17l-5-5" />
+      </svg>
+    </span>
+
+    <span className="toast-text">{toast}</span>
+  </div>
+)}
       </div>
     </section>
   );
